95c72ba3260d8918abcb994f801d840f79070831
Joasia Klasik <asklas@op.pl>
2025-04-21 16:01:19 +0200

    Format cities and add comment

--- src/implementation/Cities.java
+++ src/implementation/Cities.java
@@ -1,12 +1,22 @@
 package implementation;
+
 public class Cities {
     private double[][] distanceMatrix;
 
+    /**
+     * Constructor to initialize the distance matrix
+     * 
+     * @param size the number of cities
+     */
     public Cities(int size) {
         distanceMatrix = new double[size][size];
         initializeMatrix();
     }
 
+    /**
+     * Initialize the distance matrix with distances between cities
+     */
+    // TODO initialize distance between cites from provided params (from a file?)
     private void initializeMatrix() {
         for (int i = 0; i < distanceMatrix.length; i++) {
             for (int j = 0; j < distanceMatrix[i].length; j++) {
@@ -18,9 +28,9 @@ public class Cities {
             for (int j = 0; j < distanceMatrix[i].length; j++) {
                 if (i == j) {
                     distanceMatrix[i][j] = 0; // Distance to itself is 0
-                } else if(distanceMatrix[i][j] == -1) {
+                } else if (distanceMatrix[i][j] == -1) {
                     int dist = (i + j) * 10; // Example distance calculation
-                    distanceMatrix[i][j] = dist; 
+                    distanceMatrix[i][j] = dist;
                     distanceMatrix[j][i] = dist; // If the distance is symmetric
 
                 }
@@ -28,6 +38,9 @@ public class Cities {
         }
     }
 
+    /**
+     * Print the distance matrix
+     */
     public void printMatrix() {
         for (double[] row : distanceMatrix) {
             for (double value : row) {
@@ -37,6 +50,11 @@ public class Cities {
         }
     }
 
+    /**
+     * Get the distance matrix
+     * 
+     * @return the distance matrix
+     */
     public double[][] getMatrix() {
         return distanceMatrix;
     }
